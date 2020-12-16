89f47c75541aa745158117f7b7fcc2e5b79b4f15
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2020-12-16 04:48:27 -0300

    Incio do pojeto - geracao automatica pelo spring initializr

---
+++ src/main/java/br/com/solucitiva/agenda/AgendaPessoalApplication.java
@@ -0,0 +1,13 @@
+package br.com.solucitiva.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaPessoalApplication.class, args);
+	}
+
+}
